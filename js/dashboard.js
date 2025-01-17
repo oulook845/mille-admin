0a92a79c2863f58e72bd9ef0df1929163d95d647
jiho <audwlgh0221@google.com>
2025-01-18 05:15:08 +0900

    이용현황 그래프 수정

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,22 +2,22 @@
 function usageStatus() {
   // 데이터 준비
   const data = {
-    labels: ["January", "February", "March", "April", "May", "June"], // X축 값
+    labels: ["1.05", "1.06", "1.07", "1.08", "1.09", "1.10", "1.11"], // X축 값
     datasets: [
       {
-        label: "Sales Over Time", //데이터 세트가 무엇을 의미하는지
-        data: [25, 50, 3, 5, 20, 3], // Y축 값
-        borderColor: "rgba(75, 192, 192, 1)", // 선 색상
-        backgroundColor: "rgba(255, 99, 132, 0.2)",
-        fill: true, // 선 아래 영역을 채울지 여부
-        tension: 0.1, // 선의 곡률 조정
+        label: "", //데이터 세트가 무엇을 의미하는지
+        data: [500000, 700000, 500000, 600000, 500000, 800000, 600000, 400000, 600000, 450000, 600000, 793250], // Y축 값
+        borderColor: "#A451F7", // 선 색상
+        backgroundColor: "transparent",
+        fill: false, // 선 아래 영역을 채울지 여부
+        tension: 0.7, // 선의 곡률 조정
       },
     ], //datasets
   }; // data
 
   // 그래프 설정
   const config = {
-    type: "bar", // 그래프 유형 line, bar, pie, doughnut, radar, scatter, bubble, mixed
+    type: "line", // 그래프 유형 line, bar, pie, doughnut, radar, scatter, bubble, mixed
     data: data,
     options: {
       scales: {
@@ -25,32 +25,24 @@ function usageStatus() {
           position: "bottom", // X축을 하단에 배치
           grid: {
             // 격자선 설정
-            display: true, // 격자선 표시 여부
+            display: false, // 격자선 표시 여부
             color: "rgba(0, 0, 0, 0.1)", // 격자선 색상
             lineWidth: 1, // 격자선 두께
           },
         },
         y: {
           position: "left", // Y축을 왼쪽에 배치
-          beginAtZero: true, // Y축이 0부터 시작
+          beginAtZero: false, // Y축이 0부터 시작 (true)
           min: 0, // Y축의 최소값을 0으로 설정
-          max: 100, // Y축의 최대값을 100으로 설정
+          max: 1000000, // Y축의 최대값을 1000000으로 설정
           ticks: {
             // 축의 눈금 설정정
-            stepSize: 10, // 눈금 간격을 10으로 설정
+            stepSize: 100000, // 눈금 간격을 100000으로 설정
             font: {
               size: 14, // 글꼴 크기 설정
               family: "Arial", // 글꼴 패밀리 설정
             },
-            color: "rgba(75, 192, 192, 1)", // 눈금 글꼴 색상
-          },
-        },
-        y2: {
-          type: "linear", // 선형 Y축
-          position: "right", // 오른쪽 Y축
-          beginAtZero: true,
-          grid: {
-            drawOnChartArea: false, // 오른쪽 Y축에는 격자선 표시 안 함
+            color: "#222", // 눈금 글꼴 색상
           },
         },
       }, // scales
@@ -61,36 +53,6 @@ function usageStatus() {
       animation: {
         duration: 1000, // 애니메이션 지속 시간
       }, // animation
-
-      plugins: {
-        legend: {
-          position: "bottom", // 범례(데이터 설명) 위치 top, right, bottom, left
-          labels: {
-            font: {
-              size: 14, // 글꼴 크기
-              weight: "bold", // 글꼴 두께
-              family: "Arial", // 글꼴 패밀리
-            }, //font
-            color: "rgba(0, 0, 0, 0.7)", // 글꼴 색상
-          }, //labels
-        }, //legend
-
-        // title: {
-        //   // 차트의 제목 (차트가 무엇을 나타내는지, 차트 상단에 표시)
-        //   display: true,
-        //   text: "Sales Over Time",
-        //   font: {
-        //     size: 18, // 글꼴 크기
-        //     weight: "bold", // 글꼴 두께
-        //     family: "Arial", // 글꼴 패밀리
-        //   }, //font
-        //   color: "rgba(0, 0, 0, 0.8)", // 글꼴 색상
-        //   padding: {
-        //     top: 20, // 제목 위 여백
-        //     bottom: 20, // 제목 아래 여백
-        //   }, //padding
-        // }, //title
-      }, //plugins
     }, //options
   }; // config
 
