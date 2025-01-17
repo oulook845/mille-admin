63dc8e221b074a95f38cc8b8ce2c6114ee6a6bf7
jiho <audwlgh0221@google.com>
2025-01-18 04:56:07 +0900

    실시간 인기도서 순서변경기능 구현

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,97 +1,262 @@
-// 데이터 준비
-const data = {
-  labels: ["January", "February", "March", "April", "May", "June"], // X축 값
-  datasets: [
-    {
-      label: "Sales Over Time", //데이터 세트가 무엇을 의미하는지
-      data: [25, 50, 3, 5, 20, 3], // Y축 값
-      borderColor: "rgba(75, 192, 192, 1)", // 선 색상
-      backgroundColor: "rgba(255, 99, 132, 0.2)",
-      fill: true, // 선 아래 영역을 채울지 여부
-      tension: 0.1, // 선의 곡률 조정
-    },
-  ], //datasets
-}; // data
-
-// 그래프 설정
-const config = {
-  type: "bar", // 그래프 유형 line, bar, pie, doughnut, radar, scatter, bubble, mixed
-  data: data,
-  options: {
-    scales: {
-      x: {
-        position: "bottom", // X축을 하단에 배치
-        grid: {
-          // 격자선 설정
-          display: true, // 격자선 표시 여부
-          color: "rgba(0, 0, 0, 0.1)", // 격자선 색상
-          lineWidth: 1, // 격자선 두께
-        },
+// 이용현황 ####################################################
+function usageStatus() {
+  // 데이터 준비
+  const data = {
+    labels: ["January", "February", "March", "April", "May", "June"], // X축 값
+    datasets: [
+      {
+        label: "Sales Over Time", //데이터 세트가 무엇을 의미하는지
+        data: [25, 50, 3, 5, 20, 3], // Y축 값
+        borderColor: "rgba(75, 192, 192, 1)", // 선 색상
+        backgroundColor: "rgba(255, 99, 132, 0.2)",
+        fill: true, // 선 아래 영역을 채울지 여부
+        tension: 0.1, // 선의 곡률 조정
       },
-      y: {
-        position: "left", // Y축을 왼쪽에 배치
-        beginAtZero: true, // Y축이 0부터 시작
-        min: 0, // Y축의 최소값을 0으로 설정
-        max: 100, // Y축의 최대값을 100으로 설정
-        ticks: {
-          // 축의 눈금 설정정
-          stepSize: 10, // 눈금 간격을 10으로 설정
-          font: {
-            size: 14, // 글꼴 크기 설정
-            family: "Arial", // 글꼴 패밀리 설정
+    ], //datasets
+  }; // data
+
+  // 그래프 설정
+  const config = {
+    type: "bar", // 그래프 유형 line, bar, pie, doughnut, radar, scatter, bubble, mixed
+    data: data,
+    options: {
+      scales: {
+        x: {
+          position: "bottom", // X축을 하단에 배치
+          grid: {
+            // 격자선 설정
+            display: true, // 격자선 표시 여부
+            color: "rgba(0, 0, 0, 0.1)", // 격자선 색상
+            lineWidth: 1, // 격자선 두께
           },
-          color: "rgba(75, 192, 192, 1)", // 눈금 글꼴 색상
         },
-      },
-      y2: {
-        type: "linear", // 선형 Y축
-        position: "right", // 오른쪽 Y축
-        beginAtZero: true,
-        grid: {
-          drawOnChartArea: false, // 오른쪽 Y축에는 격자선 표시 안 함
+        y: {
+          position: "left", // Y축을 왼쪽에 배치
+          beginAtZero: true, // Y축이 0부터 시작
+          min: 0, // Y축의 최소값을 0으로 설정
+          max: 100, // Y축의 최대값을 100으로 설정
+          ticks: {
+            // 축의 눈금 설정정
+            stepSize: 10, // 눈금 간격을 10으로 설정
+            font: {
+              size: 14, // 글꼴 크기 설정
+              family: "Arial", // 글꼴 패밀리 설정
+            },
+            color: "rgba(75, 192, 192, 1)", // 눈금 글꼴 색상
+          },
         },
-      },
-    }, // scales
+        y2: {
+          type: "linear", // 선형 Y축
+          position: "right", // 오른쪽 Y축
+          beginAtZero: true,
+          grid: {
+            drawOnChartArea: false, // 오른쪽 Y축에는 격자선 표시 안 함
+          },
+        },
+      }, // scales
 
-    responsive: true, // 화면 크기 변경 시 차트 크기 자동 조정 (true)
-    maintainAspectRatio: false, // 비율을 유지하지 않음 (false)
+      responsive: true, // 화면 크기 변경 시 차트 크기 자동 조정 (true)
+      maintainAspectRatio: false, // 비율을 유지하지 않음 (false)
 
-    animation: {
-      duration: 1000, // 애니메이션 지속 시간
-    }, // animation
+      animation: {
+        duration: 1000, // 애니메이션 지속 시간
+      }, // animation
 
-    plugins: {
-      legend: {
-        position: "bottom", // 범례(데이터 설명) 위치 top, right, bottom, left
-        labels: {
-          font: {
-            size: 14, // 글꼴 크기
-            weight: "bold", // 글꼴 두께
-            family: "Arial", // 글꼴 패밀리
-          }, //font
-          color: "rgba(0, 0, 0, 0.7)", // 글꼴 색상
-        }, //labels
-      }, //legend
+      plugins: {
+        legend: {
+          position: "bottom", // 범례(데이터 설명) 위치 top, right, bottom, left
+          labels: {
+            font: {
+              size: 14, // 글꼴 크기
+              weight: "bold", // 글꼴 두께
+              family: "Arial", // 글꼴 패밀리
+            }, //font
+            color: "rgba(0, 0, 0, 0.7)", // 글꼴 색상
+          }, //labels
+        }, //legend
 
-      // title: {
-      //   // 차트의 제목 (차트가 무엇을 나타내는지, 차트 상단에 표시)
-      //   display: true,
-      //   text: "Sales Over Time",
-      //   font: {
-      //     size: 18, // 글꼴 크기
-      //     weight: "bold", // 글꼴 두께
-      //     family: "Arial", // 글꼴 패밀리
-      //   }, //font
-      //   color: "rgba(0, 0, 0, 0.8)", // 글꼴 색상
-      //   padding: {
-      //     top: 20, // 제목 위 여백
-      //     bottom: 20, // 제목 아래 여백
-      //   }, //padding
-      // }, //title
-    }, //plugins
-  }, //options
-}; // config
+        // title: {
+        //   // 차트의 제목 (차트가 무엇을 나타내는지, 차트 상단에 표시)
+        //   display: true,
+        //   text: "Sales Over Time",
+        //   font: {
+        //     size: 18, // 글꼴 크기
+        //     weight: "bold", // 글꼴 두께
+        //     family: "Arial", // 글꼴 패밀리
+        //   }, //font
+        //   color: "rgba(0, 0, 0, 0.8)", // 글꼴 색상
+        //   padding: {
+        //     top: 20, // 제목 위 여백
+        //     bottom: 20, // 제목 아래 여백
+        //   }, //padding
+        // }, //title
+      }, //plugins
+    }, //options
+  }; // config
 
-// 그래프 그리기
-const ctx = document.getElementById("usage-Status").querySelector("canvas").getContext("2d");
-const myLineChart = new Chart(ctx, config);
+  // 그래프 그리기
+  const ctx = document.getElementById("usage-Status").querySelector("canvas").getContext("2d");
+  const myLineChart = new Chart(ctx, config);
+}
+
+// 실시간 인기도서 ####################################################
+// 새로고침하면 인기도서 순위 변경
+function shuffleRank() {
+  const popular_Books = document.getElementById("popular-Books"); // 실시간 인기도서 영역
+  const popularList = popular_Books.querySelector("ol#popular-Books-list"); // 실시간 인기도서 리스트
+  const popularList_Elems = popularList.querySelectorAll("li"); //
+  const popularList_shuffleButton = popular_Books.querySelector(".data-refresh"); // 실시간 인기도서 새로고침 버튼
+
+  // 인기 도서 리스트 TOP8
+  let popularList_Array = [
+    {
+      id: 0,
+      bookName: "무의식은 어떻게 나를 설계하는가",
+      nowRank: "3",
+      transRank: "rank-Up",
+    },
+    {
+      id: 1,
+      bookName: "하루 한 장, 작지만 큰 변화의 힘",
+      nowRank: "1",
+      transRank: "rank-Down",
+    },
+    {
+      id: 2,
+      bookName: "고전이 답했다 마땅히 살아야 할 삶에 대하여",
+      nowRank: "3",
+      transRank: "rank-Up",
+    },
+    {
+      id: 3,
+      bookName: "트렌드 코리아 2025",
+      nowRank: "3",
+      transRank: "rank-Up",
+    },
+    {
+      id: 4,
+      bookName: "어른의 행복은 조용하다",
+      nowRank: "2",
+      transRank: "rank-Down",
+    },
+    {
+      id: 5,
+      bookName: "이처럼 사소한 것들",
+      nowRank: "6",
+      transRank: "rank-Up",
+    },
+    {
+      id: 6,
+      bookName: "돌이킬 수 있는",
+      nowRank: "New",
+      transRank: "New-rank",
+    },
+    {
+      id: 7,
+      bookName: "B주류 경제학",
+      nowRank: "1",
+      transRank: "rank-Hold",
+    },
+  ];
+
+  // 인기도서 기본 순위
+  function popularList_default() {
+    popularList_Elems.forEach((popularList_Elem, idx) => {
+      let bookName = popularList_Elem.querySelector(".book_name");
+      let bookRank = popularList_Elem.querySelector(".books_nowRank");
+
+      bookName.style.transition = `${0}s`;
+      bookName.style.transitionDelay = `${0}s`;
+      bookName.style.opacity = "0";
+      bookName.style.transform = "translateY(20px)";
+
+      bookRank.style.transition = `${0}s`;
+      bookRank.style.transitionDelay = `${0}s`;
+      bookRank.style.opacity = "0";
+      bookRank.style.transform = "translateY(20px)";
+    });
+  }
+
+  // 인기도서 올라오는 애니메이션
+  function popularList_upAni() {
+    popularList_Elems.forEach((popularList_Elem, idx) => {
+      let bookName = popularList_Elem.querySelector(".book_name");
+      let bookRank = popularList_Elem.querySelector(".books_nowRank");
+
+      bookName.style.transition = `${0.75}s`;
+      bookName.style.transitionDelay = `${0.1 * idx}s`;
+      bookName.style.opacity = "1";
+      bookName.style.transform = "translateY(0)";
+
+      bookRank.style.transition = `${0.75}s`;
+      bookRank.style.transitionDelay = `${0.1 * idx}s`;
+      bookRank.style.opacity = "1";
+      bookRank.style.transform = "translateY(0)";
+    });
+  }
+  popularList_upAni();
+
+  // 인기도서 요소에 데이터 입력
+  function populateList() {
+    // 인기도서 순위 랜덤
+    for (let i = popularList_Array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [popularList_Array[i], popularList_Array[j]] = [popularList_Array[j], popularList_Array[i]];
+    }
+
+    function getRandomRankStatus() {
+      const statuses = ["rank-Up", "rank-Down", "New-rank", "rank-Hold"];
+      const randomIndex = Math.floor(Math.random() * 3) + 1;
+      return statuses[randomIndex];
+    }
+    // 인기도서 책제목, 순위 입력하기
+    popularList_Elems.forEach((popularList_Elem, idx) => {
+      popularList_Elem.querySelector(".book_name").textContent = `${popularList_Array[idx].bookName}`;
+      // 랭크 유형을 가져와 변화후 재적용
+      popularList_Elem.querySelector(".books_nowRank").setAttribute("data-transRank", getRandomRankStatus());
+      let rankStatus = popularList_Elem.querySelector(".books_nowRank").getAttribute("data-transRank");
+
+      // 변화된 랭크 유형을 가져와 유형에 따라 다른 요소 넣기
+      switch (rankStatus) {
+        case "rank-Up":
+        case "rank-Down":
+          popularList_Array[idx].nowRank = Math.floor(Math.random() * 2) + 1;
+          popularList_Array[idx].nowRank = -popularList_Array[idx].nowRank;
+          break;
+        case "New-rank":
+          popularList_Array[idx].nowRank = "New";
+          break;
+        case "rank-Hold":
+        default:
+          popularList_Array[idx].nowRank = "ㅡ";
+          break;
+      }
+
+      let displayRank = popularList_Array[idx].nowRank;
+      // if (typeof displayRank === "number" && displayRank > 0) {
+      //   displayRank = `+${displayRank}`;
+      // }
+
+      popularList_Elem.querySelector(".books_nowRank").textContent = `${displayRank}`;
+    });
+  }
+  populateList();
+
+  // 새로고침시 실행할 함수
+  function shuffleAndCompare() {
+    popularList_default(); // 리스트 애니메이션 초기화
+    populateList(); // 순위 변경
+    setTimeout(popularList_upAni, 750); // 리스트 애니메이션
+  }
+  // 새로고침 버튼 누르면 업데이트
+  popularList_shuffleButton.addEventListener("click", shuffleAndCompare);
+}
+
+// #######################################
+
+usageStatus(); //#이용현황
+shuffleRank(); //#실시간 인기도서
+//#데일리
+//#유입경로
+//#콘텐츠 이용비율
+//#문의
--- js/sortable.js
+++ js/sortable.js
@@ -5,6 +5,11 @@ const items = container.querySelectorAll(".dragger");
 // columns
 columns.forEach((column) => {
   // sortable : 드래그해서 위치 옮기기
+
+  if (column.childElementCount <= 1) {
+    column.classList.add("no-drag");
+  }
+
   new Sortable(column, {
     group: "shared", // 여러 column을 하나의 그룹으로 묶음
     // animation: 150, // 애니메이션 속도
@@ -13,52 +18,58 @@ columns.forEach((column) => {
     preventOnFilter: true, // filter 조건에 맞는 요소는 드래그 차단
 
     onEnd: (evt) => {
-      console.log(evt);
       let currentColumn = evt.from; // 이벤트 일어난 column
+      // console.log(evt);
       const current_items = currentColumn.querySelectorAll(".dragger"); // 이벤트 일어난 column의 dragger만 선택
-      
 
-      if (current_items.length < 2) {
-        // 아이템이 1개인 경우, 드래그를 차단하기 위해 클래스 추가
-        for (cr_item of current_items) {
-          cr_item.classList.add("no-drag");
-        }
-      } 
-      else if (current_items.length > current_items.length - 1) {
+      if (current_items.length > current_items.length - 1) {
         for (all_item of items) {
           all_item.classList.remove("no-drag");
         } // 드래그 차단 해제
       }
+
+      oneList_dragDisable(column, currentColumn);
       scaleCheck();
     },
   });
+
+  // dragger이 하나 있을 때 선택 못하게
+  function oneList_dragDisable(column, currentColumn) {
+    for (column of columns) {
+      column.classList.remove("no-drag");
+      if (column.childElementCount <= 1) {
+        column.classList.add("no-drag");
+      }
+    }
+  }
+  // oneList_dragDisable();
 });
 
-// .draggers 크기 업데이트트
-function scaleCheck(){
+// .draggers 크기 업데이트
+function scaleCheck() {
   items.forEach((dragger) => {
     const contents = dragger.querySelector(".content");
-    
+
     // dragger 크기 자동 지정
     let dragger__scale = dragger.getAttribute("data-area");
     dragger.style.flex = dragger__scale;
-  
+
     let drg__width = dragger.offsetWidth;
     let w_value = 300;
     // 200보다 작으면 배경색상 변화
-    for(let i=0; i<items.length; i++){
+    for (let i = 0; i < items.length; i++) {
       if (drg__width < w_value) {
-        dragger.style.backgroundColor = "#333";
-        contents.style.display = "none";
-      }else{
-        dragger.style.backgroundColor = "#fff";
-        contents.style.display = "block";
+        // dragger.style.backgroundColor = "#333";
+        // contents.style.display = "none";
+      } else {
+        // dragger.style.backgroundColor = "#fff";
+        // contents.style.display = "block";
       }
     }
   });
 }
 scaleCheck();
 
-window.addEventListener("resize",function(){
+window.addEventListener("resize", function () {
   scaleCheck();
-})
+});
