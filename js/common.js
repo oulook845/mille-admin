8f0a3d157ee5811bec5aafd5a47ad83d8cfa8d9e
jiho <audwlgh0221@google.com>
2025-02-09 00:20:11 +0900

    방문 콘텐츠 시작

--- js/common.js
+++ js/common.js
@@ -1,3 +1,4 @@
+// a[href='#'] 기본클릭 이벤트 제거
 const noLinkAnchors = document.querySelectorAll("a[href='#']");
 noLinkAnchors.forEach((noLinkAnchor) => {
   noLinkAnchor.addEventListener("click", function (e) {
@@ -5,22 +6,30 @@ noLinkAnchors.forEach((noLinkAnchor) => {
   });
 });
 
+// nav 클릭시 보이는 콘텐츠 업데이트
 const mainGnb = document.getElementById("main_gnb");
 const mainGnb_links = mainGnb.querySelectorAll("li");
+const sectionLists = document.querySelectorAll(".contentList");
 const mainGnb_darkMode = mainGnb.querySelector("#dark-mode");
 const root = document.documentElement;
-
-mainGnb_links.forEach((mainGnb_link) => {
+console.log(mainGnb_links)
+mainGnb_links.forEach((mainGnb_link, idx) => {
   mainGnb_link.addEventListener("click", function (e) {
     e.preventDefault();
     for (mainGnb_link of mainGnb_links) {
       mainGnb_link.classList.remove("list-active");
     }
+    sectionLists.forEach((sectionList)=>{
+      sectionList.classList.remove("currentViewSection");
+    });
+    sectionLists[idx].classList.add("currentViewSection");
     this.classList.add("list-active");
+    console.log(this)
   });
 });
 
 
+// 다크모드 버튼 이벤트
 // mainGnb_darkMode.addEventListener("click", darkMode_TransColor);
 // let isDarkMode = true;
 // function darkMode_TransColor(){
